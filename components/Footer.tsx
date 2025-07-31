e01439fdcf7c645637d9c6ea8d7bdba0a5d1e772
End-Tech-Lab <endtechlab@gmail.com>
2025-08-01 03:43:07 +0900

    フッター微修正

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 // components/Footer.tsx
 const Footer = () => {
     return (
-      <footer className="bg-gray-100 border-t text-sm text-gray-500 mt-20">
+      <footer className="bg-gray-100 border-t text-sm text-gray-500 mt-20 pt-8 pb-6">
         <div className="max-w-6xl mx-auto px-4 py-6 text-center">
           © End-Tech-Lab. | <a href="https://store.line.me/stickershop/author/5517919/ja" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">LINEスタンプ</a>
         </div>
