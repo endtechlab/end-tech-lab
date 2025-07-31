de08e0186d43227062ea3f2ac15a124baa197cde
End-Tech-Lab <endtechlab@gmail.com>
2025-08-01 05:35:39 +0900

    諸々のレイアウト調整

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -13,7 +13,7 @@ const WorksPage: NextPage<Props> = ({ works }) => {
     <main className="max-w-6xl mx-auto px-4 py-12">
       <div>
         <h1 className="text-3xl font-bold text-gray-800 mb-12 border-b-2 border-blue-300 pb-2 inline-block max-w-full px-2">実績紹介</h1>
-        <div className="flex flex-col gap-8 max-w-3xl mx-auto">
+        <div className="flex flex-col gap-8 max-w-6xl mx-auto">
         {works.map((work) => (
           <div
             key={work.id}
