8385d5a39e2ed0826bcd8127fcefe5489349ec9a
End-Tech-Lab <endtechlab@gmail.com>
2025-08-01 05:39:04 +0900

    スマホ対応（画面遷移後の表示位置調整）

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,32 @@
 // pages/_app.tsx
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // ルート変更時にページの先頭にスクロール
+    const handleRouteChange = () => {
+      window.scrollTo(0, 0);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <Header />
       <main className="flex-1">
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </main>
       <Footer />
     </div>
