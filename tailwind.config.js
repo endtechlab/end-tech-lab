15b8cb0b6ed83494ebc462aea077c5857e8245ab
End-Tech-Lab <endtechlab@gmail.com>
2025-08-01 03:24:41 +0900

    スマホ対応（ハンバーガーメニュー修正）

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,32 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./styles/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-        "./lib/**/*.{js,ts,jsx,tsx}",
-        "./types/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./types/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 0.3s ease-out',
+      }
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-      ],
-  };
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
   
